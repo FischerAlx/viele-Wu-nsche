b5c23aaaeaaee1c819be5cdd3e5d1566fb43893c
FischerAlx <fisheralx@icloud.com>
2022-12-25 23:40:23 +0100

    init proj

--- viele Wünsche/ViewController.swift
+++ viele Wünsche/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainTextLabel: UILabel!
+    
+    var questionNumber = 0
+    var wünsche = ["Alles wird gut im Leben, besonders an der persönlichen Front!", "Freude und Glück mit viel Geld kommen hinzu!", "Das Glück verlässt dich nicht, es wird ein neues Häuschen für dich geben!", "Ein Ausflug ans Meer erwartet Sie bald!", "Langweile dich nicht, es werden neue Freunde kommen!", "Freudentränen werden fließen - ein alter Freund kehrt zu dir zurück!", "Sie werden in die Creme der Gesellschaft aufgenommen, vielleicht finden Sie einen Sponsor!", "Liebe wird deine Tage schmücken und sie werden hell sein!", ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        wünsche.shuffle()
     }
 
-
+    @IBAction func pressedMainButton(_ sender: UIButton) {
+        
+        mainTextLabel.text = wünsche[questionNumber]
+        if questionNumber + 1 < wünsche.count{
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        
+        //var letters = Set<Character>()
+        //mainTextLabel.text = "\(letters)"
+        //print("letters имеет тип Set<Character> с \(letters.count) элементами.")
+        
+        
+        
+        
+    }
+    
 }
 
---
+++ viele Wünsche/Wünsche.swift
@@ -0,0 +1,13 @@
+//
+//  Wünsche.swift
+//  viele Wünsche
+//
+//  Created by Александр Фишер on 25.12.22.
+//
+
+import Foundation
+
+
+
+
+
